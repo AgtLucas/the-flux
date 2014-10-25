9aba6db0e68f974592aa4eea3395b0774cdcf4b5
Lucas <lucas@agtlucas.com>
2014-10-25 19:44:03 -0200

    Remove from cart component updated!

--- src/js/components/app-removefromcart.js
+++ src/js/components/app-removefromcart.js
@@ -1,3 +1,18 @@
 /**
  * @jsx React.DOM
  */
+var React = require('react');
+var AppActions = require('../actions/app-actions');
+
+var RemoveFromCart = react.createClass({
+  handleClick: function () {
+    AppActions.removeItem(this.props.index);
+  },
+  render: function () {
+    return (
+      <button onClick={this.handleClick}>x</button>
+    );
+  }
+});
+
+module.exports = RemoveFromCart;
