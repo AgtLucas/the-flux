6dc1377c00d5bcfd2a74b71406ce731505f1425d
Lucas <lucas@agtlucas.com>
2014-10-25 19:39:13 -0200

    Update AddToCart component

--- src/js/components/app-addtocart.js
+++ src/js/components/app-addtocart.js
@@ -1,3 +1,18 @@
 /**
  * @jsx React.DOM
  */
+var React = require('react');
+var AppActions = require('../actions/app-actions');
+
+var AddToCart = React.createClass({
+  handleClick: function () {
+    AppActions.addItem(this.props.item);
+  },
+  render: function () {
+    return (
+      <button onClick={this.handleClick}>Add to Cart</button>
+    );
+  }
+});
+
+module.exports = AddToCart;
