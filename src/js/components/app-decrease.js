a741e89f60040001ffb2daaf7f43a1275438eec2
Lucas <lucas@agtlucas.com>
2014-10-25 19:53:17 -0200

    Update AppDecrease component

--- src/js/components/app-decrease.js
+++ src/js/components/app-decrease.js
@@ -1,3 +1,18 @@
 /**
  * @jsx React.DOM
  */
+var React = require('react');
+var AppActions = require('../actions/app-actions');
+
+var AppDecrease = React.createClass({
+  handleClick: function () {
+    AppActions.decreaseItem(this.props.index);
+  },
+  render: function () {
+    return (
+      <button onClick={this.handleClick}>-</button>
+    )
+  }
+});
+
+module.exports = AppDecrease;
