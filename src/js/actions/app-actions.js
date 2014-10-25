4474f1fac82d815e679eef398d069c0b407e8166
Lucas <lucas@agtlucas.com>
2014-10-25 18:34:28 -0200

    Change lower case to upper case

--- src/js/actions/app-actions.js
+++ src/js/actions/app-actions.js
@@ -1,36 +1,36 @@
-var appConstants = require('../constants/app-constants.js');
-var appDispatcher = require('../dispatchers/app-dispatcher.js');
+var AppConstants = require('../constants/app-constants.js');
+var AppDispatcher = require('../dispatchers/app-dispatcher.js');
 
-var appActions = {
+var AppActions = {
 
   addItem: function (item) {
-    appDispatcher.handleViewAction({
-      actionType: appConstants.ADD_ITEM,
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.ADD_ITEM,
       item: item
     });
   },
 
   removeItem: function (index) {
-    appDispatcher.handleViewAction({
-      actionType: appConstants.REMOVE_ITEM,
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.REMOVE_ITEM,
       item: index
     });
   },
 
   increaseItem: function (index) {
-    appDispatcher.handleViewAction({
-      actionType: appConstants.INCREASE_ITEM,
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.INCREASE_ITEM,
       item: index
     });
   },
 
   decreaseItem: function (index) {
-    appDispatcher.handleViewAction({
-      actionType: appConstants.DECREASE_ITEM,
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.DECREASE_ITEM,
       item: index
     });
   }
 
 };
 
-module.exports = appActions;
+module.exports = AppActions;
